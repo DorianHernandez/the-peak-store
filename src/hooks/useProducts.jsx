5d9c1f9521a716178a74f3e5a26112050b919b3a
DorianHernandez <dorianhernandez2011@gmail.com>
2025-12-29 05:23:14 -0600

    Fetch products from backend in useProducts hook
    Updated the useProducts hook to fetch products from the backend and store them in state.

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -1,14 +1,25 @@
 import { useEffect, useState } from "react";
-import products from "../data/products";
+//import products from "../data/products";
 
 export default function useProducts() {
-  const [data, setData] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setTimeout(() => {
-      setData(products);
-    }, 400);
+    console.log("Llamando al backend...");
+
+    fetch("http://localhost:8081/operador/productos")
+      .then((response) => {
+        console.log("Status:", response.status);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Datos recibidos del backend:", data);
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error("Error en fetch:", error);
+      });
   }, []);
 
-  return data;
+  return products;
 }
