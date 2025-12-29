8c955f10158893e2947fa08c9674d3993bbd5683
DorianHernandez <dorianhernandez2011@gmail.com>
2025-12-29 05:30:30 -0600

    Update Return.jsx

--- src/pages/Return.jsx
+++ src/pages/Return.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import products from "../data/products";
+//import products from "../data/products";
+import useProducts from "../hooks/useProducts";
 import "../styles/return.css";
 
 export default function Return() {
   const { id } = useParams();
+  const products = useProducts();
   const product = products.find((p) => p.id === Number(id));
 
+  if (products.length === 0) {
+    return (
+      <div className="buy_page__charge">
+        <p>Por favor espera un momento, estamos procesando su solicitud...</p>
+      </div>
+    );
+  }
+
+  if (!product) {
+    return (
+      <div className="buy_page__charge">
+        <p>Producto no encontrado</p>
+      </div>
+    );
+  }
+  
   return (
     <section className="page__return">
       <h2 className="return__title">Solicitud de devolución iniciada</h2>
