da289c163e2e3dda2709b4a0315df45df302450f
DorianHernandez <dorianhernandez2011@gmail.com>
2025-12-29 05:29:07 -0600

    Update Products.jsx

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,36 +2,34 @@ import React from "react";
 import { useState, useEffect } from "react";
 import useProducts from "../hooks/useProducts";
 import SearchBar from "../components/SearchBar";
-import ProductListDev from "../components/ProductListDev";
+import ProductList from "../components/ProductList";
 import "../styles/products.css";
 
-export default function ProductsDev() {
-    
-  const allProducts = useProducts();     
+export default function Products() {
+
+  const allProducts = useProducts();
 
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
     setProducts(allProducts);
-    }, [allProducts]);
+  }, [allProducts]);
 
-    const handleSearch = (query) => {
+  const handleSearch = (query) => {
     const lowerQuery = query.toLowerCase();
     const filtered = allProducts.filter((p) =>
-    p.name.toLowerCase().includes(lowerQuery) ||
-    p.brand.toLowerCase().includes(lowerQuery) ||
-    p.category.toLowerCase().includes(lowerQuery) ||
-    p.description.toLowerCase().includes(lowerQuery)
+      p.name.toLowerCase().includes(lowerQuery) ||
+      p.brand.toLowerCase().includes(lowerQuery) ||
+      p.category.toLowerCase().includes(lowerQuery) ||
+      p.description.toLowerCase().includes(lowerQuery)
     );
-
     setProducts(filtered);
-
   };
 
   return (
     <section className="page_products">
       <SearchBar onSearch={handleSearch} />
-      <ProductListDev products={products} />
+      <ProductList products={products} />
     </section>
   );
 }
